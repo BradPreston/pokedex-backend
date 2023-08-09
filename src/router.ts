c1c53dca223baebeb110dc9371df0912e220abf8
brad preston <bap5393@gmail.com>
2023-08-09 07:40:13 -0400

    Add getOneById handler to route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { morganMiddleware } from "./logging/morgan";
+import { getOneById } from "./handlers";
 
 export function router(app: Express) {
 	app.use(morganMiddleware);
+	app.get("/api/pokemon/:id", getOneById);
 }

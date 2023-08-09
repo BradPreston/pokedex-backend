f3ab61f6578bd42d6bede6d456efaa7be59f1525
brad preston <bap5393@gmail.com>
2023-08-09 07:39:58 -0400

    Add getOneById handler

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,7 +2,22 @@ import { Request, Response } from "express";
 import { Repo } from "./repositories";
 const repo = new Repo();
 
-export async function getOneById(req: Request, res: Response) {}
+export async function getOneById(req: Request, res: Response) {
+	try {
+		const id = parseInt(req.params.id);
+		const pokemon = await repo.oneById(id);
+		res.status(200);
+		res.json(pokemon);
+	} catch (err: any) {
+		if (err instanceof ReferenceError) {
+			res.status(404);
+			res.json({ error: err.message });
+		} else {
+			res.status(400);
+			res.json({ error: "something went wrong" });
+		}
+	}
+}
 
 export function getOneByName(req: Request, res: Response) {}
 

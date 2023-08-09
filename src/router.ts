c06bd2a15d8944b302629ae177a08c88e4c2bd52
brad preston <bap5393@gmail.com>
2023-08-09 07:24:19 -0400

    Remove crud folder. Not used

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,6 @@
 import { Express } from "express";
-import { getOneById } from "./crud/get/getOne";
 import { morganMiddleware } from "./logging/morgan";
 
 export function router(app: Express) {
 	app.use(morganMiddleware);
-	app.get("/api/pokemon/:id", getOneById);
 }

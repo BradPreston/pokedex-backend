378700c46035be5ec2cb6eb9f901c60c0a7cee78
brad preston <bap5393@gmail.com>
2023-08-09 07:59:50 -0400

    Update router reference to getOne

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import { Express } from "express";
 import { morganMiddleware } from "./logging/morgan";
-import { getOneById } from "./handlers";
+import { getOne } from "./handlers";
 
 export function router(app: Express) {
 	app.use(morganMiddleware);
-	app.get("/api/pokemon/:id", getOneById);
+	app.get("/api/pokemon/:id", getOne);
 }
